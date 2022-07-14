0a7c6618eeabe6c2b46431b3f79ae06085b815d0
Maurice <mauricenganga41@gmail.com>
2022-07-14 19:46:26 +0300

    Done with assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,22 +3,67 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [select, setSelect] = useState("All");
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    setFoods(
+      [...foods, newFood]
+    )
+  }
+  
+  const handleClick = (id) => {
+    // const filtered = foods.filter( food => food.id !== id);
+    const updated = foods.map(food => {
+      if(food.id === id){
+        return {
+          ...food,
+          heatLevel:food.heatLevel += 1
+        }
+      }
+      else{
+        return food
+      }
+      
+    })
+    setFoods(updated)
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  const filteredFood =
+    foods.filter(food => {
+      if(select === "All"){
+        return true;
+      }
+      else{
+        return food.cuisine === select;
+      }
+    })
+  
+
+  // const foodList = foods.map((food) => (
+  //   <li key={food.id} onClick={()=>handleClick(food.id)}>
+  //     {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  //   </li>
+  // ));
+  const foodList = filteredFood.map((food) => (
+    <li key={food.id} onClick={()=>handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
+
+
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+      <select name="filter" onChange={(event)=> setSelect(event.target.value)}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+</select>
     </div>
   );
 }
